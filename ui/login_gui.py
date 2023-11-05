a35e9d1a9e3ca1f9d664185c2074b896f15366a9
Clint Jonathan A. Galendez <20210021497@my.xu.edu.ph>
2023-11-05 13:00:31 +0800

    added pop-up window functionality for error message

--- ui/login_gui.py
+++ ui/login_gui.py
@@ -180,15 +180,44 @@ canvas.create_rectangle(
 
 
 def get_username_text():
+    """
+    get_username_text returns the text in the username textbox
+    :return: username_textbox.get()
+    """
     return username_textbox.get()
 
 
 def get_password_text():
+    """
+    get_password_text returns the text in the password textbox
+    :return: password_textbox.get()
+    """
     return password_textbox.get()
 
 
 def get_submit_button():
+    """
+    get_submit_button returns the submit button
+    :return: submit_button
+    """
     return submit_button
+
+
+def error_message(msg):
+    """
+    error_message creates a popup window with the error message msg
+    :param msg: the message to be displayed
+    :return: None
+    """
+    popup = Tk()
+    popup.wm_title("!")
+    popup.geometry("300x100")
+    popup.configure(bg="#925FE2")
+    label = Text(popup, text=msg, bg="#925FE2", fg="white", font=("Poppins Medium", 16 * -1))
+    label.pack(side="top", fill="x", pady=10)
+    B1 = Button(popup, text="Okay", bg="#1C1D21", fg="white", font=("Poppins Medium", 16 * -1), command=popup.destroy)
+    B1.pack()
+    popup.mainloop()
 
 
 window.resizable(False, False)
