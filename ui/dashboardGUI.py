6dc319abc7c816bdb415462e527f053179dc2530
Clint Jonathan A. Galendez <20210021497@my.xu.edu.ph>
2023-11-22 11:00:01 +0800

    refactored dashboardGUI

--- ui/dashboardGUI.py
+++ ui/dashboardGUI.py
@@ -1,232 +1,245 @@
 from pathlib import Path
-from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage, Frame
 import ui.uiManager as uiManager
 
 
-ASSETS_PATH = Path(r"assets/dashboard")
+ASSETS_PATH = Path(r"ui/assets/dashboard")
 
 
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
 
 
-window = Tk()
+def dashboard_gui_start(root):
+    dashboard_gui = Frame(root)
+    dashboard_gui.pack(fill="both", expand=True)
 
-window.geometry("937x667")
-window.configure(bg = "#FFFFFF")
+    def round_rectangle(x1, y1, x2, y2, radius=25, **kwargs):
+        points = [x1 + radius, y1,
+                  x1 + radius, y1,
+                  x2 - radius, y1,
+                  x2 - radius, y1,
+                  x2, y1,
+                  x2, y1 + radius,
+                  x2, y1 + radius,
+                  x2, y2 - radius,
+                  x2, y2 - radius,
+                  x2, y2,
+                  x2 - radius, y2,
+                  x2 - radius, y2,
+                  x1 + radius, y2,
+                  x1 + radius, y2,
+                  x1, y2,
+                  x1, y2 - radius,
+                  x1, y2 - radius,
+                  x1, y1 + radius,
+                  x1, y1 + radius,
+                  x1, y1]
 
+        return canvas.create_polygon(points, **kwargs, smooth=True)
 
-def round_rectangle(x1, y1, x2, y2, radius=25, **kwargs):
-    points = [x1 + radius, y1,
-              x1 + radius, y1,
-              x2 - radius, y1,
-              x2 - radius, y1,
-              x2, y1,
-              x2, y1 + radius,
-              x2, y1 + radius,
-              x2, y2 - radius,
-              x2, y2 - radius,
-              x2, y2,
-              x2 - radius, y2,
-              x2 - radius, y2,
-              x1 + radius, y2,
-              x1 + radius, y2,
-              x1, y2,
-              x1, y2 - radius,
-              x1, y2 - radius,
-              x1, y1 + radius,
-              x1, y1 + radius,
-              x1, y1]
+    canvas = Canvas(
+        dashboard_gui,
+        bg = "#FFFFFF",
+        height = 667,
+        width = 937,
+        bd = 0,
+        highlightthickness = 0,
+        relief = "ridge"
+    )
 
-    return canvas.create_polygon(points, **kwargs, smooth=True)
+    canvas.place(x = 0, y = 0)
+    side_board_image = PhotoImage(
+        file=relative_to_assets("image_1.png"))
+    image_1 = canvas.create_image(
+        95.84365844726562,
+        332.84375,
+        image=side_board_image
+    )
 
+    logout_image_set_1 = PhotoImage(
+        file=relative_to_assets("image_2.png"))
+    image_2 = canvas.create_image(
+        57.06329345703125,
+        606.4606323242188,
+        image=logout_image_set_1
+    )
 
-canvas = Canvas(
-    window,
-    bg = "#FFFFFF",
-    height = 667,
-    width = 937,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge"
-)
+    logout_image_set_2 = PhotoImage(
+        file=relative_to_assets("image_3.png"))
+    image_3 = canvas.create_image(
+        51.5494384765625,
+        606.5604858398438,
+        image=logout_image_set_2
+    )
 
-canvas.place(x = 0, y = 0)
-side_board_image = PhotoImage(
-    file=relative_to_assets("image_1.png"))
-image_1 = canvas.create_image(
-    95.84365844726562,
-    332.84375,
-    image=side_board_image
-)
+    canvas.create_text(
+        69.696044921875,
+        599.2577514648438,
+        anchor="nw",
+        text="Logout",
+        fill="#FFFFFF",
+        font=("Poppins Regular", 10 * -1)
+    )
 
-logout_image_set_1 = PhotoImage(
-    file=relative_to_assets("image_2.png"))
-image_2 = canvas.create_image(
-    57.06329345703125,
-    606.4606323242188,
-    image=logout_image_set_1
-)
+    canvas.create_text(
+        60.0,
+        215.0,
+        anchor="nw",
+        text="Book Management",
+        fill="#FFFFFF",
+        font=("Poppins Regular", 10 * -1)
+    )
 
-logout_image_set_2 = PhotoImage(
-    file=relative_to_assets("image_3.png"))
-image_3 = canvas.create_image(
-    51.5494384765625,
-    606.5604858398438,
-    image=logout_image_set_2
-)
+    edit_profile_image = PhotoImage(
+        file=relative_to_assets("image_4.png"))
+    image_4 = canvas.create_image(
+        64.302490234375,
+        275.3021240234375,
+        image=edit_profile_image
+    )
 
-canvas.create_text(
-    69.696044921875,
-    599.2577514648438,
-    anchor="nw",
-    text="Logout",
-    fill="#FFFFFF",
-    font=("Poppins Regular", 10 * -1)
-)
+    canvas.create_text(
+        80.44921875,
+        268.0,
+        anchor="nw",
+        text="Edit Profile",
+        fill="#FFFFFF",
+        font=("Poppins Regular", 10 * -1)
+    )
 
-canvas.create_text(
-    60.0,
-    215.0,
-    anchor="nw",
-    text="Book Management",
-    fill="#FFFFFF",
-    font=("Poppins Regular", 10 * -1)
-)
+    dashboard_image = PhotoImage(
+        file=relative_to_assets("image_5.png"))
+    image_5 = canvas.create_image(
+        61.0,
+        175.0,
+        image=dashboard_image
+    )
 
-edit_profile_image = PhotoImage(
-    file=relative_to_assets("image_4.png"))
-image_4 = canvas.create_image(
-    64.302490234375,
-    275.3021240234375,
-    image=edit_profile_image
-)
+    book_management_image = PhotoImage(
+        file=relative_to_assets("image_6.png"))
+    image_6 = canvas.create_image(
+        45.0,
+        223.486328125,
+        image=book_management_image
+    )
 
-canvas.create_text(
-    80.44921875,
-    268.0,
-    anchor="nw",
-    text="Edit Profile",
-    fill="#FFFFFF",
-    font=("Poppins Regular", 10 * -1)
-)
+    canvas.create_text(
+        77.44921875,
+        166.0,
+        anchor="nw",
+        text="Dashboard",
+        fill="#FFFFFF",
+        font=("Poppins SemiBold", 10 * -1)
+    )
 
-dashboard_image = PhotoImage(
-    file=relative_to_assets("image_5.png"))
-image_5 = canvas.create_image(
-    61.0,
-    175.0,
-    image=dashboard_image
-)
+    notification_image = PhotoImage(
+        file=relative_to_assets("image_7.png"))
+    image_7 = canvas.create_image(
+        908.4921875,
+        27.84375,
+        image=notification_image
+    )
 
-book_management_image = PhotoImage(
-    file=relative_to_assets("image_6.png"))
-image_6 = canvas.create_image(
-    45.0,
-    223.486328125,
-    image=book_management_image
-)
+    header_image = PhotoImage(
+        file=relative_to_assets("image_8.png"))
+    image_8 = canvas.create_image(
+        561.0,
+        117.00003051757812,
+        image=header_image
+    )
 
-canvas.create_text(
-    77.44921875,
-    166.0,
-    anchor="nw",
-    text="Dashboard",
-    fill="#FFFFFF",
-    font=("Poppins SemiBold", 10 * -1)
-)
+    graduation_image = PhotoImage(
+        file=relative_to_assets("image_9.png"))
+    image_9 = canvas.create_image(
+        658.0,
+        115.0,
+        image=graduation_image
+    )
 
-notification_image = PhotoImage(
-    file=relative_to_assets("image_7.png"))
-image_7 = canvas.create_image(
-    908.4921875,
-    27.84375,
-    image=notification_image
-)
+    canvas.create_text(
+        244.30859375,
+        89.64065551757812,
+        anchor="nw",
+        text="September 4,  2023",
+        fill="#FFFFFF",
+        font=("Poppins Regular", 10 * -1)
+    )
 
-header_image = PhotoImage(
-    file=relative_to_assets("image_8.png"))
-image_8 = canvas.create_image(
-    561.0,
-    117.00003051757812,
-    image=header_image
-)
+    canvas.create_text(
+        242.0,
+        113.00003051757812,
+        anchor="nw",
+        text="Welcome back, John!",
+        fill="#FFFFFF",
+        font=("Poppins SemiBold", 20 * -1)
+    )
 
-graduation_image = PhotoImage(
-    file=relative_to_assets("image_9.png"))
-image_9 = canvas.create_image(
-    658.0,
-    115.0,
-    image=graduation_image
-)
+    backpack_image = PhotoImage(
+        file=relative_to_assets("image_10.png"))
+    image_10 = canvas.create_image(
+        791.2198486328125,
+        135.65042114257812,
+        image=backpack_image
+    )
 
-canvas.create_text(
-    244.30859375,
-    89.64065551757812,
-    anchor="nw",
-    text="September 4,  2023",
-    fill="#FFFFFF",
-    font=("Poppins Regular", 10 * -1)
-)
+    canvas.create_text(
+        204.0,
+        201.0,
+        anchor="nw",
+        text="Available Books",
+        fill="#000000",
+        font=("Poppins SemiBold", 14 * -1)
+    )
 
-canvas.create_text(
-    242.0,
-    113.00003051757812,
-    anchor="nw",
-    text="Welcome back, John!",
-    fill="#FFFFFF",
-    font=("Poppins SemiBold", 20 * -1)
-)
+    bookListCanvas = round_rectangle(
+        204.0,
+        241.0,
+        917.0,
+        646.0,
+        radius=20,
+        master=dashboard_gui,
+        fill="#FFFFFF",
+        width=3,
+        outline="#925fe2")
 
-backpack_image = PhotoImage(
-    file=relative_to_assets("image_10.png"))
-image_10 = canvas.create_image(
-    791.2198486328125,
-    135.65042114257812,
-    image=backpack_image
-)
+    search_box_image = PhotoImage(
+        file=relative_to_assets("entry_1.png"))
+    search_box_bg = canvas.create_image(
+        776.5,
+        211.0,
+        image=search_box_image
+    )
+    search_box = Entry(
+        dashboard_gui,
+        bd=0,
+        bg="#F2F2F2",
+        fg="#000716",
+        highlightthickness=0
+    )
+    search_box.place(
+        x=645.0,
+        y=198.0,
+        width=263.0,
+        height=24.0
+    )
+    search_box.insert(0, "Search Books")
+    search_box.bind("<FocusIn>", lambda x: uiManager.on_entry_focus_in(search_box, "Search Books"))
+    search_box.bind("<FocusOut>", lambda x: uiManager.on_entry_focus_out(search_box, "Search Books"))
 
-canvas.create_text(
-    204.0,
-    201.0,
-    anchor="nw",
-    text="Available Books",
-    fill="#000000",
-    font=("Poppins SemiBold", 14 * -1)
-)
+    dashboard_gui.side_board_image = side_board_image
+    dashboard_gui.logout_image_set_1 = logout_image_set_1
+    dashboard_gui.logout_image_set_2 = logout_image_set_2
+    dashboard_gui.edit_profile_image = edit_profile_image
+    dashboard_gui.dashboard_image = dashboard_image
+    dashboard_gui.book_management_image = book_management_image
+    dashboard_gui.notification_image = notification_image
+    dashboard_gui.header_image = header_image
+    dashboard_gui.graduation_image = graduation_image
+    dashboard_gui.backpack_image = backpack_image
+    dashboard_gui.search_box_image = search_box_image
 
-bookListCanvas = round_rectangle(
-    204.0,
-    241.0,
-    917.0,
-    646.0,
-    radius=20,
-    fill="#FFFFFF",
-    width=3,
-    outline="#925fe2")
+    dashboard_gui.bookListCanvas = bookListCanvas
+    dashboard_gui.search_box = search_box
 
-search_box_image = PhotoImage(
-    file=relative_to_assets("entry_1.png"))
-search_box_bg = canvas.create_image(
-    776.5,
-    211.0,
-    image=search_box_image
-)
-search_box = Entry(
-    bd=0,
-    bg="#F2F2F2",
-    fg="#000716",
-    highlightthickness=0
-)
-search_box.place(
-    x=645.0,
-    y=198.0,
-    width=263.0,
-    height=24.0
-)
-search_box.insert(0, "Search Books")
-search_box.bind("<FocusIn>", lambda x: uiManager.on_entry_focus_in(search_box, "Search Books"))
-search_box.bind("<FocusOut>", lambda x: uiManager.on_entry_focus_out(search_box, "Search Books"))
-
-window.resizable(False, False)
-window.mainloop()
+    return dashboard_gui
