2bf30b135f5ee4b4512a5590381e6b79b5836237
Clint Jonathan A. Galendez <20210021497@my.xu.edu.ph>
2023-11-24 14:05:12 +0800

    added custom round_rectangle to uiManager

--- ui/uiManager.py
+++ ui/uiManager.py
@@ -6,3 +6,28 @@ def on_entry_focus_in(textbox, placeholder):
 def on_entry_focus_out(textbox, placeholder):
     if textbox.get() == "":
         textbox.insert(0, placeholder)
+
+
+def round_rectangle(canvas, x1, y1, x2, y2, radius=25, **kwargs):
+    points = [x1 + radius, y1,
+              x1 + radius, y1,
+              x2 - radius, y1,
+              x2 - radius, y1,
+              x2, y1,
+              x2, y1 + radius,
+              x2, y1 + radius,
+              x2, y2 - radius,
+              x2, y2 - radius,
+              x2, y2,
+              x2 - radius, y2,
+              x2 - radius, y2,
+              x1 + radius, y2,
+              x1 + radius, y2,
+              x1, y2,
+              x1, y2 - radius,
+              x1, y2 - radius,
+              x1, y1 + radius,
+              x1, y1 + radius,
+              x1, y1]
+
+    return canvas.create_polygon(points, **kwargs, smooth=True)
