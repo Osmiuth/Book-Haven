0b592a5c2bd4dab28291463c143415669a5acd21
markolaivar <markanthonycolaivar@gmail.com>
2023-12-01 21:02:19 +0800

    modified addBook.py

--- addBook.py
+++ addBook.py
@@ -29,7 +29,7 @@ def add_book(new_bookname, new_author, new_isbn, new_amount, new_genre, new_publ
                     ref.child(new_isbn).child("stock").set(new_stock)
                     ctypes.windll.user32.MessageBoxW(0, "Successful!", "Success!", 1)
                 else:
-                    return print('ISBN exists')
+                    return print('mo exists')
         else:
             ref.child(new_isbn).child("bookname").set(new_bookname)
             ref.child(new_isbn).child("author").set(new_author)
